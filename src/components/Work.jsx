04d40e34c6612eb170b2d8f61736de3a2ed022aa
Carol Arevalo <carevaloiag@gmail.com>
2022-05-29 12:43:25 -0600

    clean code

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,3 @@
-// import { ReactComponent as WorkIcon } from "./work.svg";
-// import { ReactComponent as SchoolIcon } from "./school.svg";
-
 import worksData from "../data/worksData";
 
 import {
@@ -11,45 +8,29 @@ import {
 import "react-vertical-timeline-component/style.min.css";
 
 function App() {
-  let workIconStyles = { background: "#06D6A0" };
-  let schoolIconStyles = { background: "#f9c74f" };
 
   return (
-    <div>
-      <h1 className="title">Timeline</h1>
+    <div className="work">
+      <h1 className="title">Work</h1>
       <VerticalTimeline>
         {worksData.map((element) => {
-          let isWorkIcon = element.icon === "work";
-          let showButton =
-            element.buttonText !== undefined &&
-            element.buttonText !== null &&
-            element.buttonText !== "";
-
           return (
             <VerticalTimelineElement
               key={element.key}
               date={element.date}
-              dateClassName="date"
-              iconStyle={isWorkIcon ? workIconStyles : schoolIconStyles}
-            //   icon={isWorkIcon ? <WorkIcon /> : <SchoolIcon />}
+              icon = {element.icon}
+              color= {element.color}
             >
               <h3 className="vertical-timeline-element-title">
                 {element.title}
               </h3>
+
               <h5 className="vertical-timeline-element-subtitle">
                 {element.location}
               </h5>
+
               <p id="description">{element.description}</p>
-              {showButton && (
-                <a
-                  className={`button ${
-                    isWorkIcon ? "workButton" : "schoolButton"
-                  }`}
-                  href="/"
-                >
-                  {element.buttonText}
-                </a>
-              )}
+
             </VerticalTimelineElement>
           );
         })}
